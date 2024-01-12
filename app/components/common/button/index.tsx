bf2185a7c869ef47abb03f6374899fbba1811ef1
DartPv <zigfraid1944@gmail.com>
2024-01-13 00:27:08 +0200

    add button component

---
+++ app/components/common/button/index.tsx
@@ -0,0 +1,28 @@
+import React, { ButtonHTMLAttributes, FC, ReactNode, useMemo } from 'react';
+import styles from './styles.module.scss';
+import classNames from 'classnames/bind';
+
+interface IProps {
+    variant?: 'default' | 'dark' | 'light';
+    size?: 'default-size' | 'small' | 'large';
+    children: ReactNode;
+    className?: string;
+}
+
+const cx = classNames.bind(styles);
+const Button: FC<IProps & ButtonHTMLAttributes<HTMLButtonElement>> = ({
+    variant = 'default',
+    size = 'default-size',
+    children,
+    className,
+    ...props
+}) => {
+    const rootStyles = useMemo(
+        () => cx([styles.root, size, variant, className]),
+        [className, size, variant],
+    );
+
+    return <button className={rootStyles} {...props}>{children}</button>;
+};
+
+export default Button;
